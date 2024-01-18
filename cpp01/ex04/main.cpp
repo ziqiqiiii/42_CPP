fe12ccd9fbb8162ac046ffe5b674c5adcfc5d083
Thong Zi Qi <ziqi.thong@gmail.com>
2024-01-18 13:20:14 +0800

    CPP01-ex04

---
+++ cpp01/ex04/main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string> 
+#include <fstream>
+#include <filesystem>
+
+using std::cout;
+using std::endl;
+using std::cerr;
+
+// int error_checking(int argc, char **argv)
+// {
+//     if (argc != 4)
+//     {
+//         std::cout << "insufficient number of input" << std::endl;
+//         return (1);
+//     }
+//     std::ifstream readFile(argv[1]);
+//     if (!readFile.is_open())
+//     {
+//         std::cerr <<"Error: File " << argv[1] << "do not exist." << std::endl;
+//         return (1);
+//     }
+//     return (0);
+// }
+
+int main(int argc, char **argv)
+{
+    std::ifstream readFile(argv[1]);
+    std::string new_file_name = std::string(argv[1]) + std::string(".replace");
+    std::ofstream outputFile(new_file_name.c_str());
+    std::string line;
+
+    if (argc != 4)
+    {
+        std::cout << "insufficient number of input" << std::endl;
+        return (1);
+    }
+    if (!readFile.is_open())
+    {
+        std::cerr <<"Error: error opening input file"<< std::endl;
+        return (1);
+    }
+    if (!outputFile.is_open())
+    {
+        std::cerr <<"Error: error opening output file"<< std::endl;
+        return (1);
+    }
+    std::string s1 = argv[2];
+    std::string s2 = argv[3];
+    while (std::getline(readFile, line))
+    {
+        size_t pos = 0;
+        while ((pos = line.find(s1, pos)) != std::string::npos)
+        {
+            line.replace(pos, s1.length(), s2);
+            pos += s1.length();
+        }
+        outputFile << line << endl;
+    }
+    readFile.close();
+    outputFile.close();
+    return (0);
+}
