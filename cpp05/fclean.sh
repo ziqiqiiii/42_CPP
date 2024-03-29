0c10451d0306ae290dbaea2ce9f00a726b4f51dd
Thong Zi qi <tzi-qi@u90z05s02.42kl.edu.my>
2024-03-29 14:00:21 +0800

    [Add] Add fclean.sh

---
+++ cpp05/fclean.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Function to clean a directory
+clean_dir() {
+  if [ -f "Makefile" ]; then
+    make -C . fclean
+  fi
+}
+
+# Loop through all directories (including subdirectories)
+find . -type d -print | while read dir; do
+  # Skip the current directory (./)
+  if [[ "$dir" != "." ]]; then
+    # Change directory and clean
+    pushd "$dir" >/dev/null
+    clean_dir
+    # Pop back to the previous directory
+    popd >/dev/null
+  fi
+done
+
+echo "Cleaning completed!"
