3ab3b8b059eafa4417e320dbeef1d0f6316ae086
ziqiqiiii <ziqi.thong@gmail.com>
2024-04-09 09:49:36 +0800

    [UPDATE] CPP07-ex00- add min && max function templates

--- cpp07/ex00/headers/whatever.hpp
+++ cpp07/ex00/headers/whatever.hpp
@@ -7,4 +7,14 @@ using std::cout;
 using std::endl;
 using std::cerr;
 
+template <typename T>
+T max(T a, T b) {
+    return (a > b) ? a : b;
+};
+
+template <typename T>
+T min(T a, T b) {
+    return (a < b) ? a : b;
+};
+
 #endif 
--- cpp07/ex00/srcs/main.cpp
+++ cpp07/ex00/srcs/main.cpp
@@ -2,7 +2,11 @@
 
 int main()
 {
-    cout << "CPP07/ex00 - Simple function templates" << endl;
+    int a = 2;
+    int b = 3;
+    cout << "a = " << a << ", b = " << b << endl;
+    cout << "max(a, b) = " << max(a, b) << endl;
+    cout << "min(a, b) = " << min(a, b) << endl;
 
     return 0;
 }
