e0dc0ed750c7455ce077dbbeafd6bfd8f3009cbf
ziqiqiiii <ziqi.thong@gmail.com>
2024-04-11 18:12:14 +0800

    [UPDATE] CPP07-ex02-update test cases

---
+++ cpp07/ex02/objs/main.d
@@ -0,0 +1,2 @@
+objs/main.o: srcs/main.cpp srcs/../headers/Array.hpp
+srcs/../headers/Array.hpp:
--- cpp07/ex02/srcs/main.cpp
+++ cpp07/ex02/srcs/main.cpp
@@ -13,11 +13,10 @@ int main(int, char**)
         numbers[i] = value;
         mirror[i] = value;
     }
-    //SCOPE
     {
 
         cout << "-----------------------------"<< endl;
-        cout << "Out of Bound Test" << endl;
+        cout << "Empty Array Test" << endl;
         cout << "-----------------------------"<< endl;
         Array<int> tmp;
         try
@@ -34,6 +33,43 @@ int main(int, char**)
         cout << endl << endl;
         cout << "Please Enter to continue" << endl;
         string input;
+        getline(std::cin, input);
+
+        cout << "------------------------------------------------------"<< endl;
+        cout << "                   Out of Bound Test" << endl;
+        cout << "------------------------------------------------------"<< endl;
+
+
+        cout << "-----------------------------"<< endl;
+        cout << "Access Negative index" << endl;
+        cout << "-----------------------------"<< endl;
+        try
+        {
+            numbers[-2] = 0;
+        }
+        catch(const std::exception& e)
+        {
+            cout << e.what() << '\n';
+            cout << "index was -2" << endl;
+            delete[] mirror;
+        }
+
+        cout << "-----------------------------"<< endl;
+        cout << "Access Index more than Size" << endl;
+        cout << "-----------------------------"<< endl;
+        try
+        {
+            numbers[MAX_VAL] = 0;
+            cout << MAX_VAL << endl;
+        }
+        catch(const std::exception& e)
+        {
+            cout << e.what() << '\n';
+            cout << "tried to access past the last element of the array" << endl;
+        }
+
+        cout << endl << endl;
+        cout << "Please Enter to continue" << endl;
         getline(std::cin, input);
 
         tmp = numbers;
@@ -77,54 +113,14 @@ int main(int, char**)
         cout << "tmp[1]:\t\t" << tmp[1] << endl << endl;
     }
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-           cout << "didn't save the same value!!" << endl;
-            return 1;
-        }
-    }
-
     cout << endl << endl;
     cout << "Please Enter to continue" << endl;
     string input;
     getline(std::cin, input);
 
-     cout << "-----------------------------"<< endl;
-    cout << "Access Negative index" << endl;
-    cout << "-----------------------------"<< endl;
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-       cout << e.what() << '\n';
-       cout << "index was -2" << endl;
-       delete[] mirror;
-    }
-    cout << endl << endl;
-    cout << "Please Enter to continue" << endl;
-    getline(std::cin, input);
-
-    cout << "-----------------------------"<< endl;
-    cout << "Access Index more than Size" << endl;
-    cout << "-----------------------------"<< endl;
-    try
-    {
-        numbers[MAX_VAL] = 0;
-        cout << MAX_VAL << endl;
-    }
-    catch(const std::exception& e)
-    {
-       cout << e.what() << '\n';
-       cout << "tried to access past the last element of the array" << endl;
-    }
-
-    cout << endl << endl;
-    cout << "Please Enter to continue" << endl;
-    getline(std::cin, input);
+    cout << "------------------------------------------------------"<< endl;
+    cout << "   Congratulations that you PASSED all the test !" << endl;
+    cout << "------------------------------------------------------"<< endl << endl;
 
     return 0;
 }
