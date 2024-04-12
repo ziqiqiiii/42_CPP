a844d4c53216028e18460363e10e6163ae777d33
ziqiqiiii <ziqi.thong@gmail.com>
2024-04-12 09:49:02 +0800

    [UPDATE] CPP08-ex00

--- cpp08/ex00/srcs/main.cpp
+++ cpp08/ex00/srcs/main.cpp
@@ -2,5 +2,22 @@
 
 int main()
 {
+    {
+        cout << "-----------------------------"<< endl;
+        cout << "Vector Test Cases" << endl;
+        cout << "-----------------------------"<< endl;
+        vector<int> vec = {1, 2, 3, 4, 5};
+        try {
+            vector<int>::iterator result_vec = easyFind(vec, 5);
+            cout << "Value found in vector ar index [" << std::distance(vec.begin(), result_vec) << endl;
+        } catch (const std::invalid_argument& e) {
+            cerr << e.what() << endl;
+        }
+    }
+    cout << endl << endl;
+	cout << "Please Enter to continue" << endl;
+	string input;
+    getline(std::cin, input);
+
     return 0;
 }
