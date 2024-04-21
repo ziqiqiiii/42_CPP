08a5533e99f0d68bf9cca6186586e6f8094c8639
Thong Zi qi <tzi-qi@u90z05s02.42kl.edu.my>
2024-04-21 18:05:35 +0800

    [UPDATE] CPP09-ex01

---
+++ cpp09/ex01/headers/RPN.hpp
@@ -0,0 +1,43 @@
+#ifndef RPN_HPP
+# define RPN_HPP
+
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+#include <climits>
+#include <exception>
+#include <string>
+#include <algorithm>
+#include <stack>
+
+using std::cout;
+using std::endl;
+using std::cerr;
+using std::string;
+using std::stack;
+
+class RPN
+{
+	private:
+		stack<int> _operants;
+	public:
+		//=========================
+        //ORTHODOX COCANICAL FORM
+        //=========================
+		RPN();
+		~RPN();
+		RPN()
+
+		//=========================
+        //MEMBER FUNCTION
+        //=========================
+		void evaluate(string expression);
+
+		//Checker
+		void isValueValid(string expression);
+
+};
+
+# endif 
---
+++ cpp09/ex01/srcs/RPN.cpp
@@ -0,0 +1,11 @@
+#include "../headers/RPN.hpp"
+
+void RPN::evaluate(string expression)
+{
+
+}
+
+void RPN::isValueValid(string expression)
+{
+	
+}
