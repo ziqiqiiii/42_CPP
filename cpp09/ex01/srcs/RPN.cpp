bc10b858921c23c87895fba739057dfb273a6134
ziqiqiiii <ziqi.thong@gmail.com>
2024-04-23 23:21:15 +0800

    [UPDATE] CPP09-ex01 update make test

--- cpp09/ex01/srcs/RPN.cpp
+++ cpp09/ex01/srcs/RPN.cpp
@@ -1,8 +1,25 @@
 #include "../headers/RPN.hpp"
 
+RPN::RPN() {}
+
+RPN::~RPN() {}
+
+RPN::RPN(const RPN& other)
+{
+    *this = other;
+}
+
+RPN& RPN::operator=(const RPN& other)
+{
+    if (this != &other)
+        this->_operants = other._operants;
+    return (*this);
+}
+
 void RPN::evaluate(string expression)
 {
     (void)expression;
+    cout << expression << endl;
 }
 
 void RPN::isValueValid(string expression)
--- cpp09/ex01/srcs/main.cpp
+++ cpp09/ex01/srcs/main.cpp
@@ -1,7 +1,13 @@
 #include "../headers/RPN.hpp"
 
-int main()
+int main(int argc, char **argv)
 {
-    cout << "Hello World" << endl;
-    return 0;
+    if (argc != 2) {
+        cerr << "Error: invalid number of arguments" << endl;
+        return EXIT_FAILURE;
+    }
+    RPN rpn;
+
+    rpn.evaluate(argv[1]);
+    return EXIT_SUCCESS;
 }   
